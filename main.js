d3fea48d119dde8026f321a8c096d1bdcd31c80c
maknetaRo <makneta@gmail.com>
2020-02-07 15:53:55 +0100

    adding scroll to top button with functionality

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+// scroll top button 
+mybutton = document.getElementById("my-btn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topButton() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
 // search fonts
 const names = document.querySelectorAll('article')
 
